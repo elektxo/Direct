4c5568274e3a9caa1194d13217c70e5b8204c44d
Adrian <adrianfuenteshernandez63@gmail.com>
2020-10-03 12:23:42 +0100

    Diseñe la pantalla de login y cree los archivos para trabajar el registro

---
+++ app/src/main/java/com/blogspot/femcomputer/direct/ResgisterActivity.kt
@@ -0,0 +1,11 @@
+package com.blogspot.femcomputer.direct
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResgisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
